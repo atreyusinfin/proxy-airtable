de72aa40f9be75352d8ec7454618e35f488b6deb
atreyu <atreyusinfinone@yahoo.es>
2022-10-07 12:18:37 -0500

    Adding Auth basic constraint.

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ import v1 from './routes/v1.js'
 import {env} from 'node:process'
 
 const cfg = {
-    port: env.PORT || 4321
+    port: env.PORT || 4321,
+    auth: env.BEARE_AUTH || ''
 }
 
 const app = express()
 app.use((req, res, next) => {
+    // Check Authorization header
+    const headers = JSON.parse(JSON.stringify(req.headers))
+    if (headers.Authorization !== cfg.auth) {
+        res.json({error: 'not found'})
+    }
+
     console.log(req.url)
+
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Content-Type', 'application/json');
     next()
 })
 
